9a51619102684f66276e7a373baaa1ae47c0ce7d
Sonia Yamile Ortega Carrillo <SoniaYamile01@Gmail.com>
2023-05-26 22:06:39 -0500

    Add files via upload
    Fundamentos en Javascript

---
+++ fundamentos/js/app.js
@@ -0,0 +1,6 @@
+console.log("Realizamos operaciones básicas");
+
+let numero1 = prompt("Ingrese un número");
+let numero2 = prompt("Ingrese otro número");
+var resultado = parseFloat(numero1) + parseFloat(numero2)
+console.log(resultado);
